d0ff4bfb843b688822b0e5d058e54717e02137e8
Kondemahesh <99584470+Kondemahesh@users.noreply.github.com>
2024-12-02 00:26:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome to iQuant YouTube Channel.\n');
+  res.send('Hello, World! Welcome to Konde Harshith & Manaswi you tube Channel.\n');
 });
 
 app.listen(port, () => {
